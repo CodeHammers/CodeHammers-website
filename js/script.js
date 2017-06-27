0a9c66b29a144a8c66d2e4aab153eaf7de69ee77
kareem <kareememad400@gmail.com>
2017-06-28 00:04:35 +0200

    basic validations + resize footer

--- js/script.js
+++ js/script.js
@@ -1,8 +1,14 @@
 $(function(){
 	$('#contact button').click(function(event) {
 		/* Act on the event */
-		$.post('/path/to/file', {param1: 'value1'}, function(data, textStatus, xhr) {
+		if($('#subject').val()!=""&& $('#name').val()!=""&& $('#email').val()!=""&& $('#message').val()!=""){
+			
+		//$.post('/path/to/file', {param1: 'value1'}, function(data) {
 			/*optional stuff to do after success */
-		});
+		//});
+		}
+		else{
+			$('#errors').removeClass('invisible');
+		}
 	});    
 });
